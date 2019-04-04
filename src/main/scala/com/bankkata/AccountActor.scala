01d1e2611ad8f9678bbea5a209824fdcd949cc14
Joaquin Caro <jcaromiq@gmail.com>
2019-04-05 00:15:52 +0200

    withdraw amount

--- src/main/scala/com/bankkata/AccountActor.scala
+++ src/main/scala/com/bankkata/AccountActor.scala
@@ -2,12 +2,14 @@ package com.bankkata
 
 import akka.actor.Actor
 
-case class Deposit(amount:Int)
+case class Deposit(amount: Int)
+case class Withdraw(amount: Int)
 
-class AccountActor extends Actor{
+class AccountActor extends Actor {
   var balance: Int = 0
 
   override def receive: Receive = {
-    case Deposit(amount) => balance += amount
+    case Deposit(amount)  => balance += amount
+    case Withdraw(amount) => balance -= amount
   }
 }
--- src/test/scala/com/bankata/BankAccountSpec.scala
+++ src/test/scala/com/bankata/BankAccountSpec.scala
@@ -2,11 +2,11 @@ package com.bankata
 
 import akka.actor.ActorSystem
 import akka.testkit.{TestActorRef, TestKit, TestProbe}
-import com.bankkata.{AccountActor, Deposit}
+import com.bankkata.{AccountActor, Deposit, Withdraw}
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 
-class BankAccountSpec (_system: ActorSystem)
-  extends TestKit(_system)
+class BankAccountSpec(_system: ActorSystem)
+    extends TestKit(_system)
     with Matchers
     with WordSpecLike
     with BeforeAndAfterAll {
@@ -18,14 +18,22 @@ class BankAccountSpec (_system: ActorSystem)
   }
 
   "An Account Actor" should {
+
     "deposit amount in a Bank Account" in {
       val actor = TestActorRef[AccountActor]
 
       actor ! Deposit(100)
 
       assert(actor.underlyingActor.balance.equals(100))
+    }
 
+    "withdraw amount in a Bank Account" in {
+      val actor = TestActorRef[AccountActor]
+
+      actor ! Deposit(1000)
+      actor ! Withdraw(100)
+
+      assert(actor.underlyingActor.balance.equals(900))
     }
   }
-
 }
